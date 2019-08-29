e0ce7b1fe60d3aa5cab7d5116ca6ad88387adf95
Jacqueline Lam <jacqueline.karin.lam@gmail.com>
2019-08-29 04:26:20 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.include?(value_to_find)
-  
-  
+  if array.include?(value_to_find)
+    return value_to_find  
+  else 
+    return nil
+  end
 end
 
 def find_max_value(array)
